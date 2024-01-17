b3d438306207d2fd073c3e4973b262db9d65397b
DarkMagician2 <139506516+DarkMagician2@users.noreply.github.com>
2024-01-17 12:11:34 -0600

    Update game.js

--- game.js
+++ game.js
@@ -110,7 +110,7 @@ class Entity {
         },delay);
     }
     //A basic rectangle collision detector
-    async static isTouching(element1, element2){
+    static async isTouching(element1, element2){
         function touching(x1,y1,w1,h1,x2,y2,w2,h2){
             if(x2>w1+x1||x1>w2+x2||y2>h1+y1||y1>h2+y2){
                 return false;
